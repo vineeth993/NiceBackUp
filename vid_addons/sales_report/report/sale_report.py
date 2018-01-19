a2a6f4a046da983e421885a4587ce26ba36f9b88
Vineeth <vineeth@vidts.in>
2018-01-19 11:28:57 +0530

    Product Wise Report

--- vid_addons/sales_report/report/sale_report.py
+++ vid_addons/sales_report/report/sale_report.py
@@ -29,6 +29,15 @@ class sale_status_report_parser(report_sxw.rml_parse):
 class sale_status_report(report_xls):
     
     def generate_xls_report(self, parser, xls_styles, data, objects, wb):
+
+
+        if data['form']['prod_or_cust'] == 'cust':
+            self.customer_pending_report(parser, xls_styles, data, objects, wb)
+        elif data['form']['prod_or_cust'] == 'prod':
+            self.product_pending_report(parser, xls_styles, data, objects, wb)
+
+    def customer_pending_report(self, parser, xls_styles, data, objects, wb):
+
         cr, uid = self.cr, self.uid
         report_name = 'Sale Summary Report' + '-' + time.strftime('%d-%m-%Y')
         ws = wb.add_sheet(report_name)
@@ -60,8 +69,7 @@ class sale_status_report(report_xls):
         
 
         sales = sale_obj.browse(cr, uid, sale_ids)
-        if sales:
-            ws.write(0, 3, "Pending Sale Order For "+str(sales[0].partner_id.name), title2)
+        ws.write(0, 3, "Pending Sale Order For "+str(data['form']['customer'][1]), title2)
 
         headers = {
             0: 'SO DATE', 1: 'SO NUMBER', 2: 'Product', 3:'Normal Disc', 4:'Additional Disc', 5:'Extra Disc',6: 'Total Quantity', 7: 'ISSUED QTY', 8: 'Pending Quantity',
@@ -119,4 +127,81 @@ class sale_status_report(report_xls):
             invoiceQty = {}
             orderLineQty = {}
             stockQty = {}
+
+    def product_pending_report(self, parser, xls_styles, data, objects, wb):
+
+        cr, uid = self.cr, self.uid
+        report_name = 'Sale Summary Report' + '-' + time.strftime('%d-%m-%Y')
+        ws = wb.add_sheet(report_name)
+        ws.panes_frozen = True
+        ws.remove_splits = True
+        ws.portrait = 0  # Landscape
+        ws.fit_width_to_pages = 1
+        row_pos = 0
+        cols = range(10)
+        for col in cols:
+            ws.col(col).width = 4000
+        title2          = xlwt.easyxf('font: height 200, name Arial, colour_index black, bold on; align: horiz centre;')
+        normal          = xlwt.easyxf('font: height 200, name Arial, colour_index black; align: horiz left;')
+        number          = xlwt.easyxf(num_format_str='#,##0;(#,##0)')
+        number2d        = xlwt.easyxf(num_format_str='#,##0.00;(#,##0.00)')
+        number2d_bold   = xlwt.easyxf('font: height 200, name Arial, colour_index black, bold on;',num_format_str='#,##0.00;(#,##0.00)')
+
+        sale_obj = self.pool.get('sale.order')
+        picking_obj = self.pool.get('stock.picking')
+        if 'object' in data:
+            sale_ids = data['object']
+        else:
+            sale_ids = sale_obj.search(cr, uid, [('state', 'not in', ('draft', 'cancel', 'done', 'shipping_except')),
+                ('date_order', '>=', data['form']['date_from']), ('date_order', '<=', data['form']['date_to']), ("order_line", "=", data['form']['product'][1]),
+                ('company_id', '=', data['form']['company_id'][0])])
+        count = 3
+        
+
+        sales = sale_obj.browse(cr, uid, sale_ids)
+        ws.write(0, 3, "Pending Sale Order For "+str(data['form']['product'][1]), title2)
+
+        headers = {
+            0: 'Partner', 1: 'Sale Order', 2: 'date', 3: 'Total Quantity', 4: 'ISSUED QTY', 5: 'Pending Quantity',
+            }
+        orderLineQty = {}
+        invoiceQty = {}
+        issuedQuan = 0
+        for header in headers:
+            ws.write(count , header, headers[header], title2)
+        count += 1
+        for sale in sales:
+            date = datetime.datetime.strptime(sale.date_order, '%Y-%m-%d %H:%M:%S').date().strftime('%d-%m-%Y')
+            for line in sale.order_line:
+                if line.product_id.id != data['form']['product'][0]:
+                    continue
+                if orderLineQty.has_key(str(sale.partner_id.name)):
+                    orderLineQty[sale.partner_id.name][0] += line.product_uom_qty
+                else:
+                    orderLineQty.update({str(sale.partner_id.name):line.product_uom_qty})
+            for invoice in sale.invoice_ids:
+                for line in invoice.invoice_line:
+                    if line.product_id.id != data['form']['product'][0]:
+                        continue                    
+                    if invoiceQty.has_key(str(line.product_id.name)):
+                        invoiceQty[str(line.product_id.name)] += line.quantity
+                    else:
+                        invoiceQty.update({str(invoice.partner_id.name):line.quantity})
+            for order in orderLineQty:
+                if invoiceQty.has_key(str(order)):
+                    issuedQuan = orderLineQty[order] - invoiceQty[order]
+                else:
+                    issuedQuan = 0
+                if issuedQuan >= 0:
+                    ws.write(count, 0, order, normal)
+                    ws.write(count, 1, sale.name, normal)
+                    ws.write(count, 2, date, normal)
+                    ws.write(count, 3, orderLineQty[order], normal)
+                    ws.write(count, 4, issuedQuan, normal)
+                    ws.write(count, 5, (orderLineQty[order] - issuedQuan), normal)
+                    count += 1
+            
+            orderLineQty = {}
+            invoiceQty = {}        
+
 sale_status_report('report.sale.status.report', 'sale.status.report', parser=sale_status_report_parser)
--- vid_addons/sales_report/wizard/sale_report.py
+++ vid_addons/sales_report/wizard/sale_report.py
@@ -2,20 +2,35 @@
 
 from openerp.osv import fields, osv
 import time
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class sale_status_report(osv.osv_memory):
     _name = 'sale.status.report'
     _description = 'Sales Status Report'
 
+    
+    def _get_default_company(self, cr, uid, context=None):
+        company_id = self.pool.get('res.users')._get_company(cr, uid, context=context)
+        if not company_id:
+            raise osv.except_osv(_('Error!'), _('There is no default company for the current user!'))
+        return company_id
+
+
     _columns = {
         'date_from': fields.date('From'),
         'date_to': fields.date('To'),
-        'customer':fields.many2one("res.partner", string="Customer")
+        'prod_or_cust':fields.selection([('prod', 'Product'), ('cust', 'Customer')], string='Product / Customer', default='cust'),
+        'customer':fields.many2one("res.partner", string="Customer"),
+        'product':fields.many2one("product.product", string="Product"),
+        'company_id':fields.many2one("res.company", string="Company")
         }
 
     _defaults = {
         'date_from': time.strftime("%Y-01-01"),
-        'date_to': time.strftime("%Y-%m-%d")
+        'date_to': time.strftime("%Y-%m-%d"),
+        'company_id':_get_default_company
         }
 
     def print_sales_report(self, cr, uid, ids, context=None):
@@ -24,11 +39,15 @@ class sale_status_report(osv.osv_memory):
             context = {}
         datas = {'ids': ids}
         datas['form'] = self.read(cr, uid, ids)[0]
-            
+        if datas['form']['prod_or_cust'] == 'cust':
+            name = str(datas['form']['customer'][1].split(']')[1]) +"_Pending Report"
+        else:
+            name = str(datas['form']['product'][1]) +"_Pending Report"             
         return { 
             'type': 'ir.actions.report.xml',
             'report_name': 'sale.status.report',
             'datas': datas,
+            'name':name
             }
 
 sale_status_report()
