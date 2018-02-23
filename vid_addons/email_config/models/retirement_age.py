2e70ad02868adbf477d287907ff10b1ad8e9d2f9
Vineeth <vineeth@vidts.in>
2018-02-23 14:39:04 +0530

    Changes in hr

--- vid_addons/email_config/models/retirement_age.py
+++ vid_addons/email_config/models/retirement_age.py
@@ -17,10 +17,34 @@ class RetirementCalculation(models.Model):
 
 	_inherit = 'hr.employee'
 
+	title = fields.Selection([('ms', 'M/s.'), 
+							  ('mr', 'Mr.'), 
+							  ('mrs', 'Mrs.'),
+							  ('ms', 'Ms.')], string="Title")
 	age = fields.Integer('Age', readonly=True, compute='_compute_retirement_year')
 	company_retire_age = fields.Integer('Company Retirement Age', readonly=True,compute='_compute_retirement_year')
 	expected_retire_year = fields.Integer('Expected Retirement Year', readonly=True, compute='_compute_retirement_year')
 	years_of_service = fields.Integer('Years Of Service', readonly=True, compute='_compute_retirement_year')
+	employee_id = fields.Char("Employee Id")
+	identification_id = fields.Char('Identification No / Mark')
+	blood_group = fields.Char('Blood Group')
+	pf_status = fields.Selection([('yes', 'Yes'), ('no', 'No')], string='Provident Fund', default='yes')
+	provident_fund_no = fields.Char(string="Provident Fund No (UAN)")
+	esic_status = fields.Selection([('yes', 'Yes'), ('no', 'No')], string='ESIC', default='yes')
+	esic_no = fields.Char('ESIC No')
+	esic_office = fields.Char('ESIC Local Office')
+	esic_dispansary = fields.Char('ESIC Dispensary')
+	tds = fields.Selection([('yes', 'Yes'), ('no', 'No')], string='TDS', default='yes')
+	tds_perc = fields.Char("TDS %")
+	qualification = fields.Selection([('ug', 'Under Graduate'), ('g', 'graduate'), ('pg', 'Post Graduate')], string='Qualification')
+	religion = fields.Char("Religion")
+	category = fields.Char("Category")
+	contact_person_1 = fields.Char("Contact Person (1st)")
+	contact_relationship_1 = fields.Char("Relationship")
+	contact_mobile_1 = fields.Char("Mob No")
+	contact_person_2 = fields.Char("Contact Person (2nd)")
+	contact_relationship_2 = fields.Char("Relationship")
+	contact_mobile_2 = fields.Char("Mob No")
 
 
 	@api.depends('company_id.retirement_age', 'birthday')
