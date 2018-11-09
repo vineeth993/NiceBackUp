be50f653adb9e567633ea25735be0cf06ce803b5
unknown <vineeth.vijayan14@gmail.com>
2018-11-09 18:00:03 +0530

    Bug fixes in sale order_type

--- vid_addons/sale_order_type/models/res_partner.py
+++ vid_addons/sale_order_type/models/res_partner.py
@@ -7,11 +7,9 @@ from openerp import fields, models, api
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+	_inherit = 'res.partner'
 
-    sale_type = fields.Many2one(
-        comodel_name='sale.order.type', string='Sale Order Type',
-        company_dependent=True)
-    sale_sub_type_id = fields.Many2many("sale.order.sub.type", "sale_order_sub_type_rel", "partner_id", "type_id", string="Sub Type")
-    purchase_type = fields.Many2one('sale.order.type', string="Purchase order type")
-    purchase_sub_type_id = fields.Many2many("sale.order.sub.type", "purchase_order_sub_type_rel", "partner_id", "type_id", string="Sub Type")
+	sale_type = fields.Many2one(comodel_name='sale.order.type', string='Sale Order Type', track_visibility='onchange')
+	sale_sub_type_id = fields.Many2one("sale.order.sub.type", string="Sub Type", track_visibility='onchange')
+	purchase_type = fields.Many2one('sale.order.type', string="Purchase order type")
+	purchase_sub_type_id = fields.Many2one("sale.order.sub.type", string="Sub Type")
