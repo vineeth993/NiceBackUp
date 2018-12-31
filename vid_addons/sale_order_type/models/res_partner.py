f2d223656cdfe16098a4abfc27dd7d8990ead476
unknown <vineeth.vijayan14@gmail.com>
2018-12-31 18:49:07 +0530

    Changes

--- vid_addons/sale_order_type/models/res_partner.py
+++ vid_addons/sale_order_type/models/res_partner.py
@@ -9,7 +9,17 @@ from openerp import fields, models, api
 class ResPartner(models.Model):
 	_inherit = 'res.partner'
 
+	@api.depends('sale_sub_type_id')
+	def _get_form_sale(self):
+		for partner in self:
+			if partner.sale_sub_type_id:
+				if partner.sale_sub_type_id.tax_categ == 'formstate' or partner.sale_sub_type_id.tax_categ == 'forminter':
+					partner.form_sale = True
+				else:
+					partner.form_sale = False
+
 	sale_type = fields.Many2one(comodel_name='sale.order.type', string='Sale Order Type', track_visibility='onchange')
 	sale_sub_type_id = fields.Many2one("sale.order.sub.type", string="Sub Type", track_visibility='onchange')
 	purchase_type = fields.Many2one('sale.order.type', string="Purchase order type")
 	purchase_sub_type_id = fields.Many2one("sale.order.sub.type", string="Sub Type")
+	form_sale = fields.Boolean("Form Sale", compute="_get_form_sale")
