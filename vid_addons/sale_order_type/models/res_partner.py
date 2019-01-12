1651ca66b279461f130b4416502bfbae1fc7a0ef
unknown <vineeth.vijayan14@gmail.com>
2019-01-12 13:37:13 +0530

    Bug fixes

--- vid_addons/sale_order_type/models/res_partner.py
+++ vid_addons/sale_order_type/models/res_partner.py
@@ -26,10 +26,11 @@ class ResPartner(models.Model):
 	purchase_sub_type_id = fields.Many2one("sale.order.sub.type", string="Sub Type")
 	form_sale = fields.Boolean("Form Sale", compute="_get_form_sale")
 
-	@api.multi
+
 	@api.onchange('purchase_type', 'sale_type')
 	def on_change_type(self):
-		if self.purchase_type or self.sale_type:
-			self.is_company = True
-		else:
-			self.is_company = False
+                for partner in self:
+			if partner.purchase_type or partner.sale_type:
+				partner.is_company = True
+			else:
+				partner.is_company = False
