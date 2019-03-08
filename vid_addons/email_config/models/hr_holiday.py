6ef26b46e810d1c2e3afd4ea304e653ef7614e23
unknown <vineeth.vijayan14@gmail.com>
2019-03-08 09:50:48 +0530

    Leavce changes

--- vid_addons/email_config/models/hr_holiday.py
+++ vid_addons/email_config/models/hr_holiday.py
@@ -12,6 +12,12 @@ class HrHoliday(models.Model):
 
 	doc_created = fields.Date("Doc Created", default=lambda x:date.today())
 	approved_date = fields.Date("Approved Date")
+	state = fields.Selection([('draft', 'To Submit'), ('cancel', 'Cancelled'),('confirm', 'sanctioned'), ('refuse', 'Refused'), ('validate1', 'Validated'), ('validate', 'Approved')],
+            'Status', readonly=True, track_visibility='onchange', copy=False,
+            help='The status is set to \'To Submit\', when a holiday request is created.\
+            \nThe status is \'To Approve\', when holiday request is confirmed by user.\
+            \nThe status is \'Refused\', when holiday request is refused by manager.\
+            \nThe status is \'Approved\', when holiday request is approved by manager.')
 
 	def holidays_validate(self, cr, uid, ids, context=None):
 
