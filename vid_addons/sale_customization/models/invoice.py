9a83e27550f64c8cc78bc2533fb4a4b3bebde3bb
unknown <vineeth.vijayan14@gmail.com>
2019-05-09 17:46:42 +0530

    sale_cust

--- vid_addons/sale_customization/models/invoice.py
+++ vid_addons/sale_customization/models/invoice.py
@@ -108,7 +108,21 @@ class AccountInvoice(models.Model):
     # extra_discount_amount = fields.Float(string='Extra Discount', readonly=True, compute='_compute_amount', track_visibility='always')
     sale_order = fields.Char("Sale Order", readonly=True, store=True)
     brand_id = fields.Many2one("product.brand", string="Product Type", readonly=True, store=True)
-    count = fields.Integer("Count")
+    state = fields.Selection([
+            ('draft','Draft'),
+            ('proforma','Pro-forma'),
+            ('proforma2','Pro-forma'),
+            ('open','Open'),
+            ('paid','Paid'),
+            ('grn','GRN Created'),
+            ('cancel','Cancelled'),
+        ], string='Status', index=True, readonly=True, default='draft',
+        track_visibility='onchange', copy=False,
+        help=" * The 'Draft' status is used when a user is encoding a new and unconfirmed Invoice.\n"
+             " * The 'Pro-forma' when invoice is in Pro-forma status,invoice does not have an invoice number.\n"
+             " * The 'Open' status is used when user create invoice,a invoice number is generated.Its in open status till user does not pay invoice.\n"
+             " * The 'Paid' status is set automatically when the invoice is paid. Its related journal entries may or may not be reconciled.\n"
+             " * The 'Cancelled' status is used when user cancel invoice.")
 
     @api.multi
     def onchange_partner_id(self, type, partner_id, date_invoice=False,
@@ -168,7 +182,8 @@ class AccountInvoice(models.Model):
                     'invoice_state':'none',
                     'picking_type_id':types[0].id,
                     'origin':self.name,
-                    'company_id':self.partner_id.parent_id.company_id.id
+                    'company_id':self.partner_id.parent_id.company_id.id,
+                    'origin':self.number
                 }
             stock_id = stock_picking_obj.sudo().create(val)
             for value in self.invoice_line:
@@ -184,7 +199,7 @@ class AccountInvoice(models.Model):
                     }
                 stock_move_obj.sudo().create(val)
             stock_id.sudo().action_confirm()
-            self.count = 1
+            self.write({'state':'grn'})
 
 class AccountInvoiceTax(models.Model):
 
