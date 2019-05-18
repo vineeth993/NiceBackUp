ecbf2e8899ac260de2fff24b686f939e330f2e25
unknown <vineeth.vijayan14@gmail.com>
2019-05-18 17:08:44 +0530

    added constrains lot

--- vid_addons/batch_wise_price/models/lot_serial.py
+++ vid_addons/batch_wise_price/models/lot_serial.py
@@ -1,6 +1,8 @@
  
 from openerp import fields, api, models, _
+from openerp.exceptions import ValidationError
 from datetime import datetime
+
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -11,6 +13,14 @@ class LotSerial(models.Model):
 
 	name = fields.Char('Batch Number', required=True, help="Unique Serial Number")
 	pricelist = fields.Many2one('product.batch.pricelist', string="Pricelist")
+
+	@api.multi
+	@api.constrains('name')
+	def lot_name_check(self):
+		for lot in self:
+			lot_id = lot.search([('name', '=', lot.name), ('product_id', '=', lot.product_id.id)])
+			if len(lot_id) > 1:
+				raise ValidationError("Batch Number already created please select that")
 
 	@api.onchange('product_id')
 	def on_change_product(self):
