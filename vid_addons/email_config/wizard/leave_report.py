f7c98976a389bd3ef16940d726c62740cc57135f
unknown <vineeth.vijayan14@gmail.com>
2019-07-02 17:38:54 +0530

    Bug Fixes

--- vid_addons/email_config/wizard/leave_report.py
+++ vid_addons/email_config/wizard/leave_report.py
@@ -8,12 +8,19 @@ class LeaveReport(models.TransientModel):
 
 	_name = "leave.report"
 
+	def _get_user(self):
+		user_id = self.env.user.id
+		employee_id = self.env['hr.employee'].search([('user_id', '=', user_id)])
+		if employee_id:
+			return employee_id[0]
+		return False
+	
 	from_date = fields.Date("From")
 	to_date = fields.Date("To")
 	category = fields.Selection([('employee', 'Employee'),
 								  ('department', 'Department'),
 								  ('all', 'All')], string="Category", default="employee")
-	employee_id = fields.Many2one('hr.employee', string="Employee", default=lambda self:self.env.user.id)
+	employee_id = fields.Many2one('hr.employee', string="Employee", default=_get_user)
 	leave_id = fields.Many2many('hr.holidays.status', "leave_employee_rel", "leave_id", "employee_id", string="Leave")
 	description = fields.Boolean('Description')
 	department_id = fields.Many2one("hr.department", string="Department")
