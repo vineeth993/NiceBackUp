9977a4b1e7666a73e8bc7552c0ad374158782444
root <root@ip-172-31-17-199.ap-south-1.compute.internal>
2019-07-22 05:10:52 +0000

    added stock.xml

--- vid_addons/sale_customization/__openerp__.py
+++ vid_addons/sale_customization/__openerp__.py
@@ -24,6 +24,7 @@ CRM/Sales Customization
         'views/base_view.xml',
         'views/quotation_report.xml',
         'views/reporting.xml',
+	'views/stock.xml'
     ],
     'installable': True,
     'auto_install': False
