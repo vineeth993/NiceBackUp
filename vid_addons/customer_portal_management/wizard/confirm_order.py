84c25093d1c9d0df9106cddb69e4a215e3031703
root <vineeth.vijayan14@gmail.com>
2019-11-18 13:37:46 +0530

    Bug Fixes

--- vid_addons/customer_portal_management/wizard/confirm_order.py
+++ vid_addons/customer_portal_management/wizard/confirm_order.py
@@ -1,5 +1,6 @@
 
 from openerp import fields, models, api, _
+from openerp.exceptions import ValidationError
 
 class PortalSaleConfrim(models.TransientModel):
 
@@ -8,7 +9,9 @@ class PortalSaleConfrim(models.TransientModel):
 	@api.multi
 	def confirm_sale(self):
 		
-		customer_order = self.env['portal.sale'].browse(self._context.get('active_ids', []))
+		customer_order = self.env['portal.sale'].browse(self._context.get('active_ids'))
+		if not customer_order.line_id:
+			raise ValidationError("Please input product to purchase")
 		if customer_order:
 			customer_order.action_confirm()
 		
